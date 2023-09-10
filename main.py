3562867ba7731240d1e646cdc2b6ffff0349de63
Evgeniy Kukarskiy <jon.79555@gmail.com>
2023-09-10 12:30:05 +0700

    finished work

--- main.py
+++ main.py
@@ -1,14 +1,27 @@
+import json
+
 import sqlalchemy
 from sqlalchemy.orm import sessionmaker
-from models import create_tables
+from models import create_tables, import_data, sales_pub
 
-DSN = 'postgresql://postgres:Tdutybq2020@localhost:5432/book_shop_db'
+sql_system = 'postgresql'
+login = 'postgres'
+password = 'Tdutybq2020'
+name_db = 'book_shop_db'
+host = '5432'
+
+DSN = f'{sql_system}://{login}:{password}@localhost:{host}/{name_db}'
 engine = sqlalchemy.create_engine(DSN)
 
 create_tables(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+with open('tests_data.json') as f:
+    data = json.load(f)
 
+import_data(session, data)
+#publisher = input()
+sales_pub(session, 'Microsoft Press')
 
 session.close()
--- models.py
+++ models.py
@@ -22,7 +22,8 @@ class Book(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     title = sq.Column(sq.String(length=100), nullable=False)
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
-    publisher = relationship(Publisher, backref='id')
+
+    publisher = relationship(Publisher, backref='books')
 
 class Stock(Base):
     __tablename__ = 'stock'
@@ -33,8 +34,8 @@ class Stock(Base):
     count = sq.Column(sq.Integer, nullable=False)
 
     CheckConstraint('count > 0', name='count_check')
-    book = relationship(Book, backref='id')
-    shop = relationship(Shop, backref='id')
+    shops = relationship(Shop, backref='stocks')
+    books = relationship(Book, backref='books')
 
 class Sale(Base):
     __tablename__ = 'sale'
@@ -47,8 +48,49 @@ class Sale(Base):
 
     CheckConstraint('price > 0', name='price_check')
     CheckConstraint('count > 0', name='count_check')
-    stock = relationship(Stock, backref='id')
+    stock = relationship(Stock, backref='stocks')
+
+def import_data(session, data: list):
+    for row in data:
+        if row['model'] == 'publisher':
+            publisher = Publisher(name=row['fields']['name'])
+            session.add(publisher)
+        elif row['model'] == 'book':
+            book = Book(title=row['fields']['title'], id_publisher=row['fields']['id_publisher'])
+            session.add(book)
+        elif row['model'] == 'shop':
+            shop = Shop(name=row['fields']['name'])
+            session.add(shop)
+        elif row['model'] == 'stock':
+            stock = Stock(id_shop=row['fields']['id_shop'],
+                          id_book=row['fields']['id_book'], count=row['fields']['count'])
+            session.add(stock)
+        elif row['model'] == 'sale':
+            sale = Sale(price=row['fields']['price'], date_sale=row['fields']['date_sale'],
+                        count=row['fields']['count'], id_stock=row['fields']['id_stock'])
+            session.add(sale)
+        session.commit()
 
 def create_tables(engine):
     Base.metadata.drop_all(engine)
     Base.metadata.create_all(engine)
+
+def sales_pub(session, name: str):
+    book = session.query(Publisher.name, Book.id,
+                         Book.title).join(Book.publisher).filter(Publisher.name == name).subquery()
+    st_book = session.query(Stock.id, Stock.id_shop,
+                            book.c.title, book.c.name).join(book,
+                                                            Stock.id_book == book.c.id).subquery()
+    sa_st_book = session.query(st_book.c.title, st_book.c.id_shop,
+                               Sale.count, Sale.price, Sale.date_sale).join(st_book,
+                                                                            Sale.id_stock == st_book.c.id).subquery()
+    sh_sa_st_book = session.query(sa_st_book.c.title, Shop.name, (sa_st_book.c.count * sa_st_book.c.price)
+                                  , sa_st_book.c.date_sale).join(sa_st_book, Shop.id == sa_st_book.c.id_shop)
+
+    for sale in sh_sa_st_book.all():
+        row = sale[0] + ' | '
+        row += sale[1] + ' | '
+        row += str(sale[2]) + ' | '
+        row += str(sale[3])
+        print(row)
+
